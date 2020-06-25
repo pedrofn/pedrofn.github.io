4b88717de981f08a66f518c91a29f192a864539e
Pedro Nogueira <phzinho38@gmail.com>
2020-06-25 09:15:18 -0300

    started; fineshed

---
+++ projeto/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title:
+      "O sindicato de trabalhadores de uma empresa sugere que o piso salarial da classe seja de R$ 1 800,00, propondo um aumento percentual fixo por cada ano dedicado ao trabalho.A expressão que corresponde à proposta salarial (s), em função do tempo de serviço (t), em anos, é s(t) = 1800 multiplicado por (1,03) elevado por t. De acordo com a proposta do sindicato, o salário de um profissional dessa empresa com 2 anos de tempo de serviço será, em reais ?",
+    artist: "Pedro Nogueira",
+    cover: "maneiro.jpg",
+    file: "FuncaoExponencial.mp3"
+  },
+  {
+    title: "Os abitantes de uma cidade bem pequena no alasca no estado dos Estado Unidos mais ao norte ja estao acostumados a passar longas noites sem ver a luz do dia em 18 de novembro de 2018 seus pouco mais de 4000 habitantes viram o ultimo por do sol do ano oportunidade seguinte ao ver a luz do dia ocorreu no dia 23 de janeiro de 2019 as 13:04 da tarde no horario local. O fenomeno discrito esta relacionado ao fato da cidade citada ter uma posição geografica condicionada pela ?",
+    artist: "Gabriel de Madureira e Guilherme Dias",
+    cover: "maneiro.jpg",
+    file: "projecoesCartograficas.mp3"
+  },
+  {
+    title: "Em uma cidade o valor total da conta de energia eletrica e obitido pelo produto entre o consumo em (Kw) e o valor da trifa do Kwh(com tibutos), adicionado à Cosip(contribuição para custeio da iluminação publica), conforme a expressão: O valor da Cosip e fixo em cada faiza de consumo Até 80kWh = 0,00; Superior a 80 ate 100 = 2,00; Superior a 100 ate 140 = 3,00; Superior a 140 ate 200 = 4,50 ",
+    artist: "Joao Pirengi",
+    cover: "maneiro.jpg",
+    file: "FuncaoLinear.ogg"
+  },
+
+];
